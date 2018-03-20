6f63ae66ad8136fea36e0d1745f34a61159e3d81
sharmasudhir1234 <37590384+sharmasudhir1234@users.noreply.github.com>
2018-03-20 22:38:04 +0530

    Add files via upload

---
+++ temp_sensor.ino
@@ -0,0 +1,17 @@
+int lm35pin=A0;
+void setup() {
+Serial.begin(9600);  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  int analogvalue;// put your main code here, to run repeatedly:
+  float temp;
+  analogvalue =analogRead(lm35pin);
+  temp=float(analogvalue)/1023;
+  temp=temp*500;
+  Serial.print("Temp");
+  Serial.print(temp);
+  Serial.println("C");
+  delay(1000);
+}
